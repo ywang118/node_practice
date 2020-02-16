71e377f149ffa5891aa03a8f4f00914147e63738
yixuan <ywang0506@gmail.com>
2020-02-16 14:20:10 -0500

    login controller

--- blog-1/src/router/user.js
+++ blog-1/src/router/user.js
@@ -1,12 +1,17 @@
+const {loginCheck} = require('../controller/user')
+const {SuccessModel, ErrorModel} = require('../model/resModel')
 const handleUserRouter = (req, res) => {
     const method = req.method
     
     //login
 
     if(method === "POST" && req.path === "/api/user/login"){
-        return {
-            msg: "this is login api"
+        const {username, password} = req.body
+        const result = loginCheck(username, password)
+        if (result){
+            return new SuccessModel( )
         }
+        return new ErrorModel("fail to login ")
     }
 }
 
