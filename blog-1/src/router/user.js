1ca69656768afb47ba84cb65f5fd02b7ffa03ffa
yixuan <ywang0506@gmail.com>
2020-02-21 17:56:26 -0500

    cookie expires

--- blog-1/src/router/user.js
+++ blog-1/src/router/user.js
@@ -1,5 +1,15 @@
 const {login} = require('../controller/user')
 const {SuccessModel, ErrorModel} = require('../model/resModel')
+
+// 获取cookie的过期时间 
+const getCookieExpires=()=>{
+    const d = new Date()
+    // d的时间设置成当前时间加24 小时
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+    console.log('d.toUTCString is, ', d.toUTCString())
+    return d.toUTCString()
+}
+
 const handleUserRouter = (req, res) => {
     const method = req.method
     
@@ -13,7 +23,7 @@ const handleUserRouter = (req, res) => {
            //RowDataPacket { username: 'lisi', realname: '李四' }
             if (data.username){
                 //操作cookie
-                 res.setHeader('Set-Cookie', `username = ${data.username}; path=/;httpOnly`)
+                 res.setHeader('Set-Cookie', `username = ${data.username}; path=/;httpOnly; expires = ${getCookieExpires()}`)
                 return new SuccessModel()
             } 
             return  new ErrorModel("fail to login ")
