345d19b9b75931834a33225e9e94c2f579ccd001
yixuan <ywang0506@gmail.com>
2020-02-22 18:05:40 -0500

    Login authentication

--- blog-1/src/router/blog.js
+++ blog-1/src/router/blog.js
@@ -1,6 +1,16 @@
 const {getList, getDetail,newBlog,updateBlog,delBlog  } = require('../controller/blog')
 const {SuccessModel, ErrorModel} = require('../model/resModel')
 
+// 统一的登录验证函数
+const loginCheck =(req)=> {
+    if (!req.session.username){
+        return Promise.resolve(
+            new ErrorModel('尚未登录')
+        )
+    }
+}
+
+
 const handleBlogRouter =(req, res) => {
      const method = req.method
      // id提到上面 
@@ -32,8 +42,13 @@ const handleBlogRouter =(req, res) => {
         //  const data = newBlog(req.body)
         //  return new SuccessModel(data)
         
-         const author = 'zhangsan' // 假数据，待开发登录时再改成真实数据
-         req.body.author= author
+         const loginCheckResult = loginCheck(req)
+         if (loginCheckResult){
+             //未登录
+             return loginCheck
+         }
+         
+         req.body.author= req.session.username
          //req.body: body: { title: '老人与海', content: '老人与海100', author: 'zhangsan' }
          const result = newBlog(req.body)
          
@@ -47,6 +62,11 @@ const handleBlogRouter =(req, res) => {
 
      // update a blog
      if (method === "POST" && req.path === "/api/blog/update"){
+        const loginCheckResult = loginCheck(req)
+        if (loginCheckResult){
+            //未登录
+            return loginCheck
+        }
         const result = updateBlog(id, req.body)
         return result.then(val=> {
             if (val) {
@@ -59,7 +79,12 @@ const handleBlogRouter =(req, res) => {
 
      // delete blog
      if (method === "POST" && req.path === "/api/blog/del"){
-        const author = 'zhangsan' // 假数据，待开发登录时再改成真实数据
+        const loginCheckResult = loginCheck(req)
+        if (loginCheckResult){
+            //未登录
+            return loginCheck
+        }
+        const author = req.session.username
         const result = delBlog(id,author)
         result.then(val=> {
             if (val) {
--- blog-1/src/router/user.js
+++ blog-1/src/router/user.js
@@ -15,8 +15,9 @@ const handleUserRouter = (req, res) => {
     
     //login
 
-    if(method === "GET" && req.path === "/api/user/login"){
-        const {username, password} = req.query
+    if(method === "POST" && req.path === "/api/user/login"){
+        const {username, password} = req.body
+        // const {username, password} = req.query
         const result = login(username, password)
 
         return result.then(data=> {
@@ -36,17 +37,17 @@ const handleUserRouter = (req, res) => {
         
     }
 
-    // 登录验证的测试
-    if (method ==="GET" && req.path === "/api/user/login-test"){
-        if (req.session.username) {
-            return Promise.resolve(
-                new SuccessModel({
-                    session: req.session
-                })
-            )
-        }
-        return Promise.resolve(new ErrorModel("尚未登录"))
-    }
+    // // 登录验证的测试
+    // if (method ==="GET" && req.path === "/api/user/login-test"){
+    //     if (req.session.username) {
+    //         return Promise.resolve(
+    //             new SuccessModel({
+    //                 session: req.session
+    //             })
+    //         )
+    //     }
+    //     return Promise.resolve(new ErrorModel("尚未登录"))
+    // }
 }
 
 module.exports = handleUserRouter
