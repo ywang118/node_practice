95c8dcd4c3e5e26222fa4ca2a24d62db2d2bfa15
yixuan <ywang0506@gmail.com>
2020-02-24 17:03:15 -0500

    sql injection

--- blog-1/src/controller/user.js
+++ blog-1/src/controller/user.js
@@ -1,9 +1,12 @@
-const {exec} = require('../db/mysql')
+const {exec,escape} = require('../db/mysql')
 
 const login = (username,password)=>{
+    username = escape(username)
+    password = escape(password)
     const sql = `
-    select username, realname from users where username = '${username}' and password='${password}'
+    select username, realname from users where username =${username} and password=${password}
     `
+    console.log('sql is', sql)
     // select 返回的都是数组
     return exec(sql).then(rows=> {
         return rows[0] || {}
